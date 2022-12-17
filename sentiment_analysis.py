6003c02ec19d4a6ff3c10d0bef901456ccaaa0a3
mie-h <56993977+mie-h@users.noreply.github.com>
2022-12-17 15:27:11 -0800

    sentimet analysis using gpt3

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,10 +1,8 @@
 import openai
+from decouple import config
+import argparse
 
-
-openai.api_key=''
-
-
-
+openai.api_key=config('OPENAI_API_KEY')
 
 def generate_prompt(text):
     return """This is a Sentence sentiment classifier
@@ -21,18 +19,36 @@ def generate_prompt(text):
         Sentence: {}
         Sentiment:""".format(text)
 
+def process_output(resp):
+    output_txt = resp['choices'][0]['text']
+    sentiment = output_txt.split('\n')[0]
+    return sentiment
+
+def sentiment_analysis(text):
+    print(f"The given text is: { text }")
+
+    prompt = generate_prompt(text)
+
+    response = openai.Completion.create(
+        engine="davinci",
+        prompt=prompt,
+        temperature=0.6,
+        max_tokens=60,
+        top_p=1.0,
+        frequency_penalty=0.5,
+        presence_penalty=0.0,
+        stop=["###"])
+
+    
+    sentiment = process_output(response)
+    print(f"Sentiment: { sentiment }")
 
 
-prompt = generate_prompt("Hello I am not feeling well today.")
-print(prompt)
+def main():
+    parser = argparse.ArgumentParser(description='Process sentiment analysis.')
+    parser.add_argument('--text')
+    args = parser.parse_args()
+    sentiment_analysis(args.text)
 
-response = openai.Completion.create(
-    engine="davinci",
-    prompt=prompt,
-    temperature=0.6,
-    max_tokens=60,
-    top_p=1.0,
-    frequency_penalty=0.5,
-    presence_penalty=0.0,
-    stop=["###"])
-print (response)
+if __name__ == "__main__":
+    main()
